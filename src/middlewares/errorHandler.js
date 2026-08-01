3f420df51757996aa18f5882aef2dc017787494b
Solangeaimery98-boop <Solangeaimery98@icloud.com>
2026-08-01 17:27:43 -0300

    Handle PostgreSQL errors with custom responses

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,27 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  res.status(500).json({
-    message: "Internal server error",
-  });
+  switch (err.code) {
+    case "23505":
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+
+    case "23503":
+      return res.status(400).json({
+        message: "Referenced author does not exist",
+      });
+
+    case "23502":
+      return res.status(400).json({
+        message: "Required fields are missing",
+      });
+
+    default:
+      return res.status(500).json({
+        message: "Internal server error",
+      });
+  }
 };
 
 export default errorHandler;
