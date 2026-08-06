8ad68ac6ad5d254eea3bbde98ec9e0bb8ee2d037
Solangeaimery98-boop <Solangeaimery98@icloud.com>
2026-08-06 20:26:54 -0300

    adding raylway deploy

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,7 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
